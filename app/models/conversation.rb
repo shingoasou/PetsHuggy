77eb9609d6e9306ee69959114c36fa1c7b9d0bd5
shingoasou <s.a_agribrain@icloud.com>
2019-02-21 13:38:18 +0000

    プレースホルダを使用するに修正

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -16,6 +16,7 @@ class Conversation < ActiveRecord::Base
   end
 
   scope :between, -> (sender_id,recipient_id) do
-    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
+    # where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
+    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", {sender_id: params[:sender_id], recipient_id: params[:recipient_id]})
   end
 end
